6146a5ead4d54d8dab1a1888d82786cb28f3e9a2
santhi-radiant <santhi.g@radiantcloud.co>
2022-03-22 13:58:45 +0530

    commit2

--- Module2/m2_01.sql
+++ Module2/m2_01.sql
@@ -7,4 +7,22 @@
 --SELECT DATABASE();
 ------------------------------------------------------------------
 
+
+--CREATE TABLE cameras(model_name VARCHAR(30),
+                     quantity INT   
+                        );
+--DESC cameras;
+--DROP TABLE cameras;
+--CREATE TABLE canon_cameras(model_name VARCHAR(30),quantity INT);
+--DESC canon_cameras;
+
+
+--insert into canon_cameras(model_name,quantity)values("70-D",12),("80D",19
+),("E0S-R",25),("E0S-R5",80),("E0R-R6",50);
+
+--
+mysql> select model_name,quantity from canon_cameras;
+
+mysql> select model_name,quantity from canon_cameras where quantity>=50;
+
 */
