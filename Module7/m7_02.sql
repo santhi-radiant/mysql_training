3e5b5362858cc1f8a28680a37a93df0913b6f08d
santhi-radiant <santhi.g@radiantcloud.co>
2022-03-23 14:47:49 +0530

    final

---
+++ Module7/m7_02.sql
@@ -0,0 +1,35 @@
+/*
+
+Join
+
+Q1: get me all order id, customer name and date when ordre was placed.
+*/
+
+/*
+
+--show tables;
+--desc customers;
+--desc orders;
+
+*/
+
+
+/*
+
+
+select Orders.OrderId,Customers.CustomerName,Orders.OrderDate
+FROM Orders
+JOIN Customers ON Orders.CustomerID=Customers.CustomerID LIMIT 5;
+
+
+
+*/
+
+
+
+
+
+
+
+
+
